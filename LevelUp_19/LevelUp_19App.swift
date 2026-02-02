168ed519cfec4e024b8869b476f2eeeac575db58
Hunny <mws.optimistic2000@gmail.com>
2026-02-02 13:10:58 +0300

    Initial Commit

---
+++ LevelUp_19/LevelUp_19App.swift
@@ -0,0 +1,17 @@
+//
+//  LevelUp_19App.swift
+//  LevelUp_19
+//
+//  Created by Nuha  on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct LevelUp_19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
