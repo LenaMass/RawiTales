f47bc937f0e3a913012269490c6f35da12ef5203
Hunny <mws.optimistic2000@gmail.com>
2026-02-15 00:40:52 +0300

    Connected settings page with Main Page

--- View/LevelUp_19App.swift
+++ View/LevelUp_19App.swift
@@ -13,15 +13,20 @@ import SwiftData
 @main
 struct LevelUp_19App: App {
     @Environment(\.modelContext) private var modelContext
+    @StateObject private var settings = AppSettings()
     
     var body: some Scene {
         WindowGroup {
             PagesViewer()
+            
+            
                 .background{
                     Image("NightDay_Background")
-                    
-                    
                 }
+            
+                .environmentObject(settings) // Inject it here
+                .preferredColorScheme(settings.appearance == .dark ? .dark : .light)
+            
                 .modelContainer(for: [Story.self, WordBankItem.self])
         }
         
