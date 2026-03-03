1b7a4fc81c69ac766fa326efa127895dd50d49d9
LenaMass <109649035+LenaMass@users.noreply.github.com>
2026-03-04 01:27:53 +0300

    Glowy widget :D

--- View/MainPage/CostumeWidget.swift
+++ View/MainPage/CostumeWidget.swift
@@ -6,11 +6,13 @@ struct HeroRingWidget: View {
     var body: some View {
         ZStack {
             ZStack {
+              
                 ForEach(vm.segments) { seg in
                     Circle()
                         .trim(from: seg.from, to: seg.to)
                         .stroke(seg.color, style: StrokeStyle(lineWidth: vm.lineWidth, lineCap: .round))
                         .rotationEffect(.degrees(-90))
+                        .shadow(color: seg.color.opacity(0.8), radius: 12, x: 0, y: 0)
                 }
 
                 ForEach(vm.segments) { seg in
@@ -19,17 +21,20 @@ struct HeroRingWidget: View {
                         let x = cos(rad) * vm.iconRadius
                         let y = sin(rad) * vm.iconRadius
 
-                        RingIcon(symbol: seg.icon)
+                        RingIcon(symbol: seg.icon, color: seg.color)
                             .offset(x: x, y: y)
                     }
                 }
             }
 
+           
             Circle()
                 .fill(Color.clear)
-//                .frame(width: vm.centerSize, height: vm.centerSize)
                 .overlay(
                     Image("grandpa")
+//                        .resizable()
+                        .scaledToFit()
+                        .shadow(color: .white.opacity(0.5), radius: 3.5, x: 0, y: 0)
                 )
         }
         .frame(width: vm.ringWidth, height: vm.ringHeight)
@@ -38,6 +43,7 @@ struct HeroRingWidget: View {
 
 private struct RingIcon: View {
     let symbol: String
+    let color: Color
 
     var body: some View {
         Image(systemName: symbol)
@@ -50,6 +56,7 @@ private struct RingIcon: View {
             .overlay {
                 Circle().strokeBorder(.white.opacity(0.18), lineWidth: 1)
             }
+            .shadow(color: color.opacity(0.8), radius: 8, x: 0, y: 0)
     }
 }
 
@@ -59,5 +66,3 @@ private struct RingIcon: View {
         HeroRingWidget(vm: .preset)
     }
 }
-
-
